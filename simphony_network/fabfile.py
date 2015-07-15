4c095b1485bd145a97173dc03fd91fc62a9aabf5
Mehdi Sadeghi <sade@iwm.fraunhofer.de>
2015-07-15 16:08:24 +0200

    Add fabfile

---
+++ simphony_network/fabfile.py
@@ -0,0 +1,43 @@
+"""
+This module is part of simphony-network package.
+
+Here the public API of this package is defined.
+"""
+import os
+
+from fabric.api import cd, env, prefix, run, task, settings
+from fabric.operations import put
+
+env.hosts = ['pc-p115']
+
+
+@task
+def setup_env():
+    code_dir = '~/.virtualenvs/simphony'
+    with settings(warn_only=True):
+        if run("test -d %s" % code_dir).failed:
+            run('mkdir -p %s' % code_dir)
+            with cd('~/.virtualenvs/simphony'):
+                run('curl -O https://pypi.python.org/packages/source/v/virtualenv/virtualenv-13.1.0.tar.gz')
+                run('tar xzvf virtualenv-13.1.0.tar.gz')
+                run('rm -rf virtualenv-13.1.0.tar.gz')
+                run('python virtualenv-13.1.0/virtualenv.py .')
+                run('rm -rf virtualenv-13.1.0')
+
+
+@task
+def deploy():
+    with prefix('. ~/.virtualenvs/simphony/bin/activate'):
+        with cd('~/.virtualenvs/simphony'):
+            abspath_requirements = os.path.abspath('../requirements.txt')
+            put(abspath_requirements, '~/.virtualenvs/simphony')
+            run('pip install -r requirements.txt')
+        with cd('~/.virtualenvs/simphony/simphony-network'):
+            run('python setup.py install')
+
+
+@task
+def start():
+    with cd('~/.virtualenvs/simphony'):
+        with prefix('. ./bin/activate'):
+            run('simphony')
